6d32de24d21a1fb25516993f5464a1d6694055cb
mgalkin1 <166807265+mgalkin1@users.noreply.github.com>
2024-04-21 11:18:40 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+#immutable_var = 1,2,3,True, 'f'
+#print (immutable_var)
+#immutable_var [0] = 200 # кортеж не поддерживает обращение по элементам.
+#print (immutable_var)
+mutable_list = ['Nastya', 'Masha', 'Vika', 1, 2, True]
+print ( mutable_list)
+mutable_list [0] = 'Jenya'
+print (mutable_list)
